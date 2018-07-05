ef259c4e896a613a9554d6e86f8ab3efb12c3d41
赵国庆 <Guoqing.Zhao@cicc.com.cn>
2018-07-05 18:36:12 +0800

    fix 压缩图片

--- test.sh
+++ test.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
 
-echo "kagen" | sudo -S -v;
-echo "kagen" | sudo -S -v;
-echo "kagen" | sudo -S -v;
-echo "kagen" | sudo -S -v;
-
-echo "kagen" | sudo -H pip list;
-echo "kagen" | sudo -H pip3 list;
--- zippng.sh
+++ zippng.sh
@@ -1,41 +1,131 @@
-#! /usr/local/bin/bash
-
-LOGFILE="/Users/zhaoguoqing/Desktop/outputImages.log"
-touch $LOGFILE
+#!/usr/bin/env bash
 
 function fix_file_size_string() {
 	size=$1
 	size=$(echo "${size}" | awk '{ split( "B KB MB GB TB PB" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } printf "%.2f %s", $1, v[s] }')
-	return size
+	echo $size
 }
 
 function read_dir() {
 	OLDIFS=$IFS
-	IFS=$(echo -en "\n\b")  
+	IFS=$(echo -en "\n\b")
+	OUTPUTFILE=$2 
+	PNGCount=0
+	SUCCESSZIP=0
+	ERRORZIP=0
+	ZIPQUALITY=$3
 	for file in `ls $1`
 	do 
 		ALLPATH=$1"/"$file
 		if [ -d $ALLPATH ]
 		then 
-			read_dir $ALLPATH
+			subresult=$(read_dir $ALLPATH)
+			allCount=$(echo $subresult | cut -d '|' -f 1)
+			successCount=$(echo $subresult | cut -d '|' -f 2)
+			errorcount=$(echo $subresult | cut -d '|' -f 3)
+			PNGCount=$((PNGCount+allCount))
+			SUCCESSZIP=$((SUCCESSZIP+successCount))
+			ERRORZIP=$((ERRORZIP+errorcount))
 		else 
 			extension=${file##*.}
 			extension=$(echo $extension | tr 'a-z' 'A-Z')
 			if [ $extension = "PNG" ]
 			then
+				PNGCount=$((PNGCount+1))
                 oldSize=$(wc -c "${ALLPATH}" | awk '{print $1}')
-                oldSizestr=$(fix_file_size_string oldSize)
-                pngquant -f --ext .png --quality 90 $ALLPATH
-                newSize=$(wc -c "${ALLPATH}" | awk '{print $1}')
-                newSizestr=$(fix_file_size_string newSize)
-                zipPercent=$(echo "$newSize $oldSize" | awk '{printf ("%.2f",($2-$1)/$2*100)}')
-				echo "$ALLPATH -- $oldSizestr >>> $newSizestr  压缩率$zipPercent%" >> $LOGFILE
+                oldSizestr=$(fix_file_size_string $oldSize)
+                pngquant -f --ext .png --quality $ZIPQUALITY $ALLPATH
+                echo $?
+                if [[ $? -ne 0 ]]; then
+                	ERRORZIP=$((ERRORZIP+1))
+                else
+                	newSize=$(wc -c "${ALLPATH}" | awk '{print $1}')
+                	newSizestr=$(fix_file_size_string $newSize)
+                	zipPercent=$(echo "$newSize $oldSize" | awk '{printf ("%.2f",($2-$1)/$2*100)}')
+					echo "$ALLPATH -- $oldSizestr >>> $newSizestr  压缩率$zipPercent%" >> $OUTPUTFILE
+					SUCCESSZIP=$((SUCCESSZIP+1))
+                fi
 			fi
 		fi
 	done
-	IFS=$OLDIFS			
+	IFS=$OLDIFS	
+	echo "$PNGCount|$SUCCESSZIP|$ERRORZIP"
 }
 
-read_dir $1 2>$LOGFILE
+function install_pngquant() {
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	sudo chown -R $(whoami) $(brew --prefix)/*
+	brew install pngquant
+}
+
+if [[ ! $(which brew) ]]; then
+	install_pngquant
+fi
+
+
+LOGFILE="/Users/$(echo `whoami`)/Desktop/outputImages.log"
+INPUTFILE=""
+ZIPQUALITY=90
+
+while getopts ":i:q:l:h" opt; do
+	case $opt in
+		i )
+			temp_var="$OPTARG"
+			if [[ $temp_var =~ ^.. ]]; then
+				temp_var=$(echo $temp_var | sed "s~^..~$(pwd)~g")
+			elif [[ $temp_var =~ ^. ]]; then
+				temp_var=$(echo $temp_var | sed "s~^.~$(pwd)~g")
+			fi
+			INPUTFILE="$temp_var"
+			;;
+		q )
+			ZIPQUALITY=$OPTARG
+			;;
+		l )
+			temp_var="$OPTARG"
+			temp_var="$OPTARG"
+			if [[ $temp_var =~ ^.. ]]; then
+				temp_var=$(echo $temp_var | sed "s~^..~$(pwd)~g")
+			elif [[ $temp_var =~ ^. ]]; then
+				temp_var=$(echo $temp_var | sed "s~^.~$(pwd)~g")
+			fi
+			LOGFILE=$OPTARG
+			;;
+		h )
+			echo "用法:"
+			echo
+			echo "    $ /Path/To/script/script.sh 参数"
+			echo
+			echo "参数:"
+			echo "    -i       图片所在路径"
+			echo "    -q       图片压缩的质量, 默认90(1-100)"
+			echo "    -l       日志输出文件, 可直观的查看压缩信息, 默认在 ~/Desktop/outputImages.log"
+			echo "    -h       帮助信息"
+			exit 0
+			;;
+		\? )
+			echo "Invalid option: -$OPTARG"
+			;;
+	esac
+done
+
+if [[ $INPUTFILE = "" ]]; then
+	echo "请输入图片所在路径"
+	exit 1
+fi
+
+
+echo "" >> $LOGFILE
+echo "" >> $LOGFILE
+echo "" >> $LOGFILE
+
+echo "开始压缩PNG图片: $(echo `date '+%Y-%m-%d %H:%M:%S'`)" >> $LOGFILE
+echo "压缩路径: $INPUTFILE" >> $LOGFILE
+echo "日志路径: $LOGFILE" >> $LOGFILE
+
+zip_result=$(read_dir $1 $LOGFILE $ZIPQUALITY)
+
+echo "压缩PNG图片完毕: $(echo `date '+%Y-%m-%d %H:%M:%S'`), 图片数量:$(echo $zip_result | cut -d '|' -f 1), 成功:$(echo $zip_result | cut -d '|' -f 2), 失败:$(echo $zip_result | cut -d '|' -f 3)" >> $LOGFILE
+
 open $LOGFILE
 
